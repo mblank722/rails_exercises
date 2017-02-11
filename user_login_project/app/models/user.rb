c215b8fa77ebb223d95f1b130f5a35045d6dce41
mblank722 <mitchellblank@sbcglobal.net>
2017-02-10 18:47:06 -0800

    new user validations

--- user_login_project/app/models/user.rb
+++ user_login_project/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :first_name, :last_name, presence:true, length:{in 2..255}, case_sensitive: true
+
+  validates :email,  :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+
+  validates :age,  presence:true
+  validates_numericality_of: :age, greater_than_or_equal_to: 10, less_than: 150
 end
