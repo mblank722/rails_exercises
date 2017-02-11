868d7205f3ed4fee955e401a8b50f284e84c0b7d
mblank722 <mitchellblank@sbcglobal.net>
2017-02-11 13:24:17 -0800

    next v

--- user_login_project/app/models/user.rb
+++ user_login_project/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
 
-  validates :first_name, :last_name presence:true, length:{in 2..255}, case_sensitive: true
+  validates :first_name presence:true, length:{in 2..255}, case_sensitive: true
 
   validates :email,  :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
 
