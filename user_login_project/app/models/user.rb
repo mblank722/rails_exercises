169742c8e6ab0639b5c0a59410547ffb967374b1
mblank722 <mitchellblank@sbcglobal.net>
2017-02-11 14:25:14 -0800

    next v

--- user_login_project/app/models/user.rb
+++ user_login_project/app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name,  presence: true, length: { minimum: 2 }, length: { maximum: 255 }
 
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }, length: { maximum: 255 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }, length: { maximum: 255 },
 
 
-  validates :age,  presence:true, numericality: { only_integer: true }
+  validates :age,  presence:true, numericality: { only_integer: true, :greater_than_or_equal_to 10 , :less_than 10 }
   # validates_numericality_of :age, greater_than_or_equal_to: 10, less_than: 150
 end
