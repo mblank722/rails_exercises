0ab57b55456f9fad716070d17cf657ed0c3ca3ec
mblank722 <mitchellblank@sbcglobal.net>
2017-02-21 18:24:02 -0800

    next v

--- user_model_controller/config/routes.rb
+++ user_model_controller/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+# alias mkm='python manage.py migrate'
+get '/' => 'users#index'
+
+get '/' => 'users#index'
+
+get 'say/hello/michael' => 'views#say_hello_michael'
