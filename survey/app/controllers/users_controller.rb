9023c13f79e04c3b0f81cca313096190652b07fb
mblank722 <mitchellblank@sbcglobal.net>
2017-02-28 16:06:40 -0800

    next v

--- survey/app/controllers/users_controller.rb
+++ survey/app/controllers/users_controller.rb
@@ -4,7 +4,24 @@ class UsersController < ApplicationController
   end
 
   def show
+#insert user
+    user = User.new(name:"", email:"not_valid")
+
+# populate user data for display
+
+
+#check for validation errors:
+# => if they exist send to show page for display
+    if !user.valid?
+      flash[:error] = "You have errors"
+      redirect_to '/users/' #pathing will be explained later
+    else
+      flash[:success] = "You did it!"
+      redirect_to '/users/'
+    end
+
     params
     render json: params
   end
+
 end
