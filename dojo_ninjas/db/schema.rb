d0df8f4929569fa9baafc0c1f96a3e5bf62e865f
mblank722 <mitchellblank@sbcglobal.net>
2017-03-01 14:52:27 -0800

    next v

--- dojo_ninjas/db/schema.rb
+++ dojo_ninjas/db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301223238) do
+ActiveRecord::Schema.define(version: 20170301225004) do
 
   create_table "dojos", force: :cascade do |t|
     t.string   "name"
     t.string   "city"
     t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ninjas", force: :cascade do |t|
+    t.integer  "dojo_id"
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
